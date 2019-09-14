3e68b79e8aa12b91d0c37952755a6c9cdb087928
HarpreetBains94 <hazbains20@gmail.com>
2019-09-14 11:30:34 +0100

    Attempt to get ELMo test working

---
+++ testing/ELMo/test-elmo.py
@@ -0,0 +1,50 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import pandas as pd
+import keras
+import gensim
+
+from keras.models import Model
+from keras.layers import Input, Dense, Lambda
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+config = tf.ConfigProto( device_count = {'GPU': 1} ) 
+sess = tf.Session() 
+keras.backend.set_session(sess)
+sess.run(tf.global_variables_initializer())
+sess.run(tf.tables_initializer())
+
+# Fetch pre trained vecotrs
+elmo_embed = hub.Module("./module_elmo2", trainable=False)
+
+# Set up training data
+trainData = pd.read_csv('../training-data.csv', sep=',', encoding='latin-1')
+train_y = trainData['val'].values
+train_x_raw = trainData['tweet'].values
+
+# Set up test data
+testData = pd.read_csv('../test-data.csv', sep=',', encoding='latin-1')
+test_y = testData['val'].values
+test_x_raw = testData['tweet'].values
+
+def ELMoEmbedding(x):
+    return elmo_embed(tf.squeeze(tf.cast(x, tf.string)), signature="default", as_dict=True)["default"]
+
+input_text = Input(shape=(1,), dtype=tf.string)
+embedding = Lambda(ELMoEmbedding, output_shape=(1024, ))(input_text)
+dense = Dense(256, activation='relu')(embedding)
+pred = Dense(1, activation='sigmoid')(dense)
+model = Model(inputs=[input_text], outputs=pred)
+optimizer = Adam(lr=1e-3)
+model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+model.fit(train_x_raw, train_y, epochs=50, batch_size=512)
+result = model.evaluate(test_x_raw, test_y)
+print("Accuracy: {0:.2%}".format(result[1]))
